6b642c7c3803b37448c263b230031cc8b833cdc4
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2025-06-20 14:34:34 -0400

    Move models to `roboplan_example_models` package (#7)

---
+++ roboplan_example_models/include/roboplan_example_models/resources.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <dlfcn.h>
+#include <filesystem>
+#include <string>
+
+namespace roboplan_example_models {
+
+/// @brief Provides compile time access to the resources install directory.
+std::filesystem::path get_install_prefix();
+
+/// @brief Provides compile time access to the resources shared directory for
+/// accessing robot models or other resource files.
+std::filesystem::path get_package_share_dir();
+
+} // namespace roboplan_example_models
